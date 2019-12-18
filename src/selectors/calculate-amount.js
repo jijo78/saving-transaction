3834e97953349f3f2c6e027d65a48e103cebeda1
Giuseppe DeMarino <giuseppe.demarino@sainsburys.co.uk>
2019-12-18 12:25:45 +0000

    fixed spelling mistake

--- src/selectors/calculate-amount.js
+++ src/selectors/calculate-amount.js
@@ -1,5 +1,5 @@
 export const calculateTotalExpenditure = value => {
-  // check if the is array off integers, some checks if array values are numbers
+  // check if the is an array of integers, the some method checks if array values are numbers
   if (Array.isArray(value) && !value.some(isNaN)) {
     return value.reduce((accumulator, currentValue) => accumulator + currentValue, 0).toFixed(2);
   }
